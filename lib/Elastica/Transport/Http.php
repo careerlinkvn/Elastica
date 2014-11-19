c0efdad0c9f21427dafdc4675939e9c7d9893551
Kitamura Satoshi <with.no.parachute@gmail.com>
2014-11-19 11:52:24 +0700

    Allows Elastica to use gzip encoding

--- lib/Elastica/Transport/Http.php
+++ lib/Elastica/Transport/Http.php
@@ -62,6 +62,7 @@ class Elastica_Transport_Http extends Elastica_Transport_Abstract
         curl_setopt($conn, CURLOPT_CUSTOMREQUEST, $request->getMethod());
         curl_setopt($conn, CURLOPT_FORBID_REUSE, 0);
         curl_setopt($conn, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($conn, CURLOPT_ENCODING, 'gzip, deflate');
 
         $this->_setupCurl($conn);
 
